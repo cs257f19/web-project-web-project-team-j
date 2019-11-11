657a9d1b602b782d59dac3755f90cc7bd4632ba2
hansonc2 <38167376+hansonc2@users.noreply.github.com>
2019-11-11 16:51:24 -0600

    a2

--- flaskApp/webapp.py
+++ flaskApp/webapp.py
@@ -49,7 +49,7 @@ def get_results():
             queryResult = ds.getScoreBelow(scoreHigh)
             comments.append(queryResult)
 
-        print(len(comments))
+        print(comments)
 
     return render_template('resultsTemplate.html', comments=comments)
 
